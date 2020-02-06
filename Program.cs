8799df9bced3b2c33688985b53e11a4fd2ccade9
Marek Gorski <52073127+GorskiMarekGM@users.noreply.github.com>
2020-02-06 09:13:44 +0100

    Add files via upload

--- Program.cs
+++ Program.cs
@@ -7,15 +7,17 @@ namespace MonteCarlo
         static void Main(string[] args)
         {
             Console.WriteLine("Provide 3 tasks");
-            Console.WriteLine("Task |Best Scenario| Average | Worst scenario");
+            Console.WriteLine("Task | Worst Scenario | Average | Best Scenario");
 
             int worst = 0, best = 0, average = 0, examples = 13;
             
             //Set range of bruckets
             int range = 5;
-
+        
             int[,] intNumbers = Menu.MainMenu();
             int[,] rangeMatrix = new int[2,range];
+
+            Display.DisplayMatrix(rangeMatrix,2,range);
 
             for (int i=0; i<intNumbers.GetLength(0); i++)
             {
@@ -24,10 +26,14 @@ namespace MonteCarlo
                 worst += intNumbers[i,2];
             }
 
+            RandomPlan.GenerateRandomPlan(10, intNumbers);
+            RandomPlan.estimationDisplay();
+
             examples = Display.SetExamples();
 
             //Monte Carlo
-            int[] inum = Array_Operations.GenerateRandomArray(examples, best, worst);
+            //int[] inum = Array_Operations.GenerateRandomArray(examples, best, worst);
+            int[] inum = RandomPlan.NRandomPlans(examples, intNumbers);
 
             average = Array_Operations.ArrayAverage(inum);
 
@@ -35,17 +41,12 @@ namespace MonteCarlo
 
             Display.DisplayMatrix(intNumbers, intNumbers.GetLength(0), intNumbers.GetLength(1));
 
-            //Display array of random numbers
-            /* foreach (int s in inum)
-            {
-                Console.Write(s+ ", ");
-            }
-            Console.WriteLine();*/
+           
 
             rangeMatrix = Array_Bucketing.Buckets(inum, range, best, worst);
 
-           // Console.WriteLine("Ranges: ");
-           // Display.DisplayMatrix(rangeMatrix, 2, range);
+            Console.WriteLine("Ranges: ");
+            Display.DisplayMatrix(rangeMatrix, 2, range);
 
 
             Console.WriteLine("Probability of finishing the plan in: ");
---
+++ RandomPlan.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MonteCarlo
+{
+    static class RandomPlan
+    {
+        static private int[] estimation;
+        
+        private static void setEstimation(int col)
+        {
+            estimation = new int[col];
+        }
+
+        public static void GenerateRandomPlan(int n,int[,] arr)
+        {
+            Random randNum = new Random();
+            setEstimation(n);
+
+            for (int j = 0; j < n; j++)
+            {
+                for (int i = 0; i < arr.GetLength(0); i++)
+                {
+                    int random = randNum.Next(0, arr.GetLength(1));
+                    estimation[j] += arr[i, random];
+                }
+            }
+        }
+
+        public static void estimationDisplay()
+        {
+            foreach (var item in estimation)
+            {
+                Console.WriteLine("Estimated:" + item);
+            }
+        }
+    }
+}
