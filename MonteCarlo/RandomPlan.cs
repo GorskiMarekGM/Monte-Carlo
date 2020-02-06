86d3d39221c5be0702a22555f6b05409614393b3
Marek Gorski <52073127+GorskiMarekGM@users.noreply.github.com>
2020-02-06 23:04:32 +0100

    Add files via upload

---
+++ MonteCarlo/RandomPlan.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MonteCarlo
+{
+    static class RandomPlan
+    {
+        static public int[] estimation, worst, best;
+        
+        private static void setTables(int col)
+        {
+            estimation = new int[col];
+            worst = new int[col];
+            best = new int[col];
+        }
+
+        public static int[] GenerateRandomPlan(int n,int[,] arr)
+        {
+            Random randNum = new Random();
+            setTables(n);
+
+            for (int j = 0; j < n; j++)
+            {
+                for (int i = 0; i < arr.GetLength(0); i++)
+                {
+                    int random = randNum.Next(0, arr.GetLength(1));
+                    estimation[j] += arr[i, random];
+                    worst[j] += arr[i, arr.GetLength(1)-1];
+                    best[j] += arr[i, 0];
+                }
+            }
+            return estimation;
+        }
+
+        public static void estimationDisplay()
+        {
+            foreach (var item in estimation)
+            {
+                Console.WriteLine("Estimated:" + item);
+            }
+        }
+    }
+}
